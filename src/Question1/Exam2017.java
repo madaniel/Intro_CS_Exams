ce6f25caec494ca35d30d5c0ad5ed012faec39cd
Daniel Madar <m.a.daniel@gmail.com>
2019-06-29 13:18:18 +0300

    - adding ways

---
+++ src/Question1/Exam2017.java
@@ -0,0 +1,31 @@
+package Question1;
+
+public class Exam2017 {
+
+    /**
+     * Question 2017b A3 84
+     * @param k
+     * @param n
+     * @return
+     */
+    public static int ways(int k, int n){
+        // Stop conditions
+        if(k == 0 && n == 0)
+            return 1; // Found a way
+
+        if(k == 0)
+            return 0; // Did not find a way
+
+        int stepRight = ways(k-1, n-1); // Decreasing the distance - moving toward number n
+        int stepLeft = ways(k-1, n+1);  // Increasing the distance - moving backward number n
+        return  stepLeft + stepRight;
+    }
+
+    /**
+     * Here you can test the function above with debugger and see how it works     *
+     */
+    public static void main(String[] args) {
+        System.out.println(ways(4, 2));
+    }
+
+}
